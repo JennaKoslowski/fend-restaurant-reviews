fb0cfdba3aae554927a920be33412c9ea12fe7d1
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-09-25 19:56:30 -0500

    add map API

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamVubmFrb3Nsb3dza2kiLCJhIjoiY2ptaWUwYXd0MDN6aDNwbW93aWdmcTZvZyJ9.ZFsh13yz2sIo-_6js5LMdA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
