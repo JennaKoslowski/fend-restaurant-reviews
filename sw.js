3f5f6e172c7696203c7836285ae18dd4a5380b5d
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-10-01 20:31:30 -0500

    install service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+//help from https://www.youtube.com/watch?time_continue=3&v=ksXwaWHCW6k
+self.addEventListener('install', (event) => {
+	console.log("Service Worker is installed");
+})
